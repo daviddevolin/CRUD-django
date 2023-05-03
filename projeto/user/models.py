338da3a79dcb7344dfce8b13d8d4f3e931df8e99
david santos <56547716+daviddevolin@users.noreply.github.com>
2023-05-03 09:02:03 -0300

    comentários

--- projeto/user/models.py
+++ projeto/user/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+#criando model user
 class User (models.Model):
     nome = models.CharField(max_length=100)
 
+#tratando a froma como é exibido o user 
     def __str__(self):
         return self.nome
--- projeto/user/views.py
+++ projeto/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import User
 
-
+# retornando todos users para o index.html
 def home(request):
     users = User.objects.all
     return render(request,"index.html",{"users":users})
