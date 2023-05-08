f3dfd6bf030959f9deedba438f4ed2427c4cdc89
david santos <56547716+daviddevolin@users.noreply.github.com>
2023-05-08 20:51:23 -0300

    updtae view

--- projeto/user/views.py
+++ projeto/user/views.py
@@ -3,6 +3,7 @@ from .models import User
 
 # retornando todos users para o index.html
 def home(request):
+    #varrendo o objeto
     users = User.objects.all
     return render(request,"index.html",{"users":users})
 # Create your views here.
